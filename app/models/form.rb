e96d5adb7155cf98ea91d77d78c4ac8c4f0eb48c
uesteibar <uesteibar@gmail.com>
2015-11-29 20:35:45 +0100

    validate and test form model

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,4 +2,6 @@ class Form < ActiveRecord::Base
 	has_many :options
 	has_many :answers
 
+	validates_presence_of 	:typeform_id, :typeform_url
+	validates_uniqueness_of :typeform_id, :typeform_url
 end
---
+++ spec/models/form_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe Form, type: :model do
+  before(:each) do
+    @form_params = {
+      typeform_id: "ab44c7",
+      typeform_url: "https://forms.typeform.io/to/ab44c7",
+    }
+  end
+
+  describe "#create" do
+    it "creates a form when all params are given" do
+      expect do
+        Form.create(@form_params)
+      end.to change(Form.all, :count).by 1
+
+      expect(Form.last.id).not_to be_falsy
+      expect(Form.last.typeform_id).to eq @form_params[:typeform_id]
+    end
+
+    it "does not create a form when typeform_id is missing" do
+      @form_params[:typeform_id] = nil
+
+      expect do
+        Form.create(@form_params)
+      end.to change(Form.all, :count).by 0
+    end
+
+    it "does not create a form when typeform_url is missing" do
+      @form_params[:typeform_url] = nil
+
+      expect do
+        Form.create(@form_params)
+      end.to change(Form.all, :count).by 0
+    end
+  end
+end
